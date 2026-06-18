3eb36a00e1ef11c83f6f90ae23ea4b6f022c64e9
SagarSawlani <sawlanisagar1@gmail.com>
2026-06-18 16:20:47 +0530

    fCORS Issue Fix

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://hireband.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
